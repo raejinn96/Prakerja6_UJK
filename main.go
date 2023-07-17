0d09ccdeacc5e2fe2c4dbeba488319454dbc81ce
raejinn96 <smlekmew@gmail.com>
2023-07-17 21:19:42 +0700

    "Penambahan ECHO ke main.go"

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
-import (
-	// "fmt"
-)
+import "github.com/labstack/echo/v4"
+
+// "fmt"
 
 type Users struct{
 	Names []string
 	Country []string
+	Category []int
+	Interest []string
 }
 
 func main() {
 
+	e := echo.New()
 }
