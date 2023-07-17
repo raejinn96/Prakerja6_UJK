3c10f0d43132c441271bfe4a0ba9012a18b87f59
raejinn96 <smlekmew@gmail.com>
2023-07-17 22:01:52 +0700

    penambahan Endpoint "usersData"

--- main.go
+++ main.go
@@ -5,20 +5,30 @@ import "github.com/labstack/echo/v4"
 // "fmt"
 
 type Users struct{
-	Names []string
-	Country []string
-	Category []int
-	Interest []string
+	Names string
+	Country string
+	Category int
+	Interest string
 }
 
 func main() {
 
 	e := echo.New()
 	
-	e.GET("/profile", profile)
+	e.GET("/profile", respond)
+	e.GET("/users", usersData)
+
 	e.Start(":8000")
 }
 
-func profile(c echo.Context) error {
+func usersData(c echo.Context) error {
+	var user []Users 
+	user = append(user, Users{"Aslew", "Cambodia", 3, "Technology"})
+	user = append(user, Users{"Jino", "Singapore", 1, "Entertainment"})
+
+	return c.JSON(200, user) 
+}
+
+func respond(c echo.Context) error {
 	return c.JSON(200, "Good") 
 }
